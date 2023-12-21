c84248f48306a111b481ffcda5fb4b209295c8c1
Mahdi <m.hassani4951383@gmail.com>
2023-12-21 08:26:54 +0330

    test rectangle

--- ArduinoSnake/ArduinoSnake.ino
+++ ArduinoSnake/ArduinoSnake.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
+Adafruit_PCD8544 display = Adafruit_PCD8544(13, 11, 9, 10, 8);
+void setup()   {
+  display.begin();
+  // init done
+  display.clearDisplay();
+  display.display();
+  display.setContrast(50);
+  
+  // Draw a rectangle
+  display.drawRect(0, 0, 83, 47, BLACK);
+  display.display();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
